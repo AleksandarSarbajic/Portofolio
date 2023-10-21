c9244e8a2051d37ab4ccf544a62e70d09440732a
Aleksandar <sleasarbajic@gmail.com>
2023-10-21 20:31:16 +0200

    form

--- src/ui/Contact.jsx
+++ src/ui/Contact.jsx
@@ -19,9 +19,14 @@ function Contact() {
       <StyledText>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </StyledText>
-      <StyledForm name="contact" data-netlify="true" method="POST">
-        <input type="text" id="fullName" />
-        <button>dasd</button>
+      <StyledForm
+        name="contact"
+        data-netlify="true"
+        method="post"
+        onSubmit="submit"
+      >
+        <input type="text" id="name" name="first-name" />
+        <button type="submit">dasd</button>
       </StyledForm>
     </StyledContainer>
   );
