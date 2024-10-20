d2ee54aa8c3c685a80c19b85d7bdacc546334563
Aleksandar <sleasarbajic@gmail.com>
2024-10-20 18:49:11 +0200

    Bug book

--- src/pages/TheBugBook.jsx
+++ src/pages/TheBugBook.jsx
@@ -120,20 +120,20 @@ function TheBugBook() {
         other users posts. Phase 3 was to create a profile page where user can
         see his posts and edit his profile. Phase 4 was to enable users to like,
         comment posting, bookmark posts and follow other users. Phase 5 was to
-        enable users to see his notifications, bookmarks and search for other
-        users and content. And Phase 6 is to enable users that they can use
-        messages and chat with other users.
+        enable users to see their notifications, bookmarks and search for other
+        users and content. Phase 6 is to enable users so they can use messages
+        and chat with other users.
       </StyledSubHeading>
       <StyledSubHeading $space>
         The most effective approach I found to implement these 6 phases is to
         create a backend with Prisma and NextJS API routes so I can enable login
-        features that are crucial. After that to create a main page where users
-        access everything that they need. Then to create a profile page where
-        the user can manage his following, followers and edit his profile.
-        Afterwards, to create a functional messaging system where users can
-        chat. And when all of that is done to create a notification system that
-        will trigger on likes, comments and follows while messages have their
-        own notifications.
+        features that are crucial, after that develop a main page where users
+        access everything that they need. Then generate a profile page where the
+        user can manage his following, followers and edit his profile;
+        Afterwards, build a functional messaging system where users can chat.
+        And when all of that is done to create a notification system that will
+        trigger on likes, comments and follows while messages have their own
+        notifications.
       </StyledSubHeading>
       <StyledProjectBox>
         <StyledStack>
@@ -172,24 +172,24 @@ function TheBugBook() {
           <StyledSubHeading $big>
             I wanted to use NextJS 15 so I can practice my backend skills with
             new technologies that I learned, and I think that NextJS is perfect
-            for social media application. Prisma was chosen because I wanted to
-            use a postgres database from vercel and how easy is to connect to
-            it.
+            for social media application. I chose Prisma because I wanted to use
+            a postgres database from vercel and how easy is to connect to it.
           </StyledSubHeading>
 
           <StyledSubHeading $big $space>
-            For the whole auth logic I used lucide and for creating users
-            prisma. For validation of inputs I used Zod. For data mutation and
-            fetching, I chose ReactQuery and ky. For uploading images I used
-            uploadthing, and for editing images I chose react cropper and reacts
-            image file resizer. For styling the application I used TailWindCSS
-            with radix-ui and tip tap. Radix ui provides some quality components
-            that are easy to use and tiptap provides good editor for creating a
-            post. For the whole messaging logic i was using stream chat who
-            provides hosted chat component that is so customizable and saves a
-            lot of time implementing it . And for linkifying # links and profile
-            I used to react linkify it. Vercel is also an obvious choice for
-            deployment because of its speed and reliability for NextJS projects.
+            For the whole auth logic I used lucia and for creating users I
+            worked with Prisma. For validation of inputs I used Zod. For data
+            mutation and fetching, I chose ReactQuery and ky. For uploading
+            images I used uploadthing, and for editing images I chose react
+            cropper and reacts image file resizer. For styling the application I
+            used TailWindCSS with radix-ui, and tip tap. Radix ui provides high
+            quality components that are easy to use and tiptap provides a good
+            editor for creating a post. For the whole messaging logic i was
+            using stream chat who provides hosted chat component that is so
+            customizable and saves a lot of time implementing it . For
+            linkifying # links and profile I used to react linkify it. Vercel is
+            also an obvious choice for deployment because of its speed and
+            reliability for NextJS projects.
           </StyledSubHeading>
         </div>
       </StyledProjectBox>
@@ -248,19 +248,19 @@ function TheBugBook() {
             Problems and Thought Process
           </StyledProjectHeading>
           <StyledSubHeading $big>
-            Like most projects, there is always some problem on your journey, I
+            Like most projects, there is always some problem on your journey. I
             was stuck in different places, but the most difficult area was in
             the making everything work together. Optimistic data mutations with
-            ReactQuery, were a lot a difficult area, but I managed to make it
-            work smoothly.
+            ReactQuery, were a lot difficult area, but I managed to make it work
+            smoothly.
           </StyledSubHeading>
 
           <StyledSubHeading $big $space>
             Making chat work was also a difficult area and something completely
             new for me. I had to read a lot of docs because it was my first time
-            using it. As well for the application the be the fastest as possible
-            I was making components as reusable as possible and with help of a
-            tailwind with redix-ui I managed to do that.
+            using it. As well for the application to be the fast as possible I
+            was making components as reusable as they could be and with help of
+            a tailwind with redix-ui, I managed to do that.
           </StyledSubHeading>
         </div>
       </StyledProjectBox>
@@ -281,10 +281,10 @@ function TheBugBook() {
         I could spend all day describing the lessons that I learned while
         working on this project, but the most important ones involved in my
         newfound understanding of NextJS, Stream Chat, Lucia, Prisma, backend
-        logic and so much more. As the largest project I have ever worked on, I
-        learned a lot of new technologies and how to implement them in a real
-        project. And with this project i think i have a good understanding of a
-        fullstack application.
+        logic and so much more. As this is the largest project I have ever
+        worked on, I learned a lot of new technologies and how to implement them
+        in a real project, and with this project i think i have a good
+        understanding of a fullstack application.
       </StyledSubHeading>
       <StyledProjectHeading $medium $margin>
         Other Projects
